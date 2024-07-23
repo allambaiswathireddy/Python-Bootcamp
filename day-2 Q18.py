9ae19ccc54293eeb741104c49e4e4862c7bb9aba
allambaiswathireddy <165119728+allambaiswathireddy@users.noreply.github.com>
2024-07-23 21:42:47 +0530

    Add files via upload
    Python-Bootcamp(Day-2)

---
+++ day-2 Q18.py
@@ -0,0 +1,11 @@
+#given an space seperated integer list find the avg of elements present in the even index
+list=list(map(int,input().split(",")))
+sum=0
+count=0
+n=len(list)
+for i in range(len(list)):
+    if(i%2==0):
+        sum+=list[i]
+        count+=1
+print(sum/count)
+        
