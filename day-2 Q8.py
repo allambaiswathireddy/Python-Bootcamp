2433db7d5cbb718605da2adbac50b7485153dc65
allambaiswathireddy <165119728+allambaiswathireddy@users.noreply.github.com>
2024-07-23 21:36:56 +0530

    Add files via upload
    Python-Bootcamp(Day-2)

---
+++ day-2 Q8.py
@@ -0,0 +1,14 @@
+my_list=list(map(int,input().split("@")))
+choice=int(input())
+print(*my_list)
+if(choice==1):
+    my_list.append(99)
+    print(my_list)
+elif(choice==2):
+    my_list.pop(0)
+    print(my_list)
+elif(choice==3):
+    my_list.sort()
+    print(my_list)
+elif(choice==4):
+    print("hello")
